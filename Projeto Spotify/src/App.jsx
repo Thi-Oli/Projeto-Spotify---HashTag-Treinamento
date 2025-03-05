fa7b924d91885a6495dabf8145b850d076c2fa29
Thiago Oliveira <geovanithiago08@gmail.com>
2025-03-04 21:18:06 -0300

    Aula 1 - Projeto Spotify

---
+++ Projeto Spotify/src/App.jsx
@@ -0,0 +1,16 @@
+// Arrow Function -> rafce <-
+
+import React from "react";
+import Header from "./components/Header";
+import Main from "./components/main";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default App;
---
+++ Projeto Spotify/src/App2.jsx
@@ -0,0 +1,33 @@
+// Teste de CSS direto do Navegador
+
+// element.style {
+//   background: antiquewhite;
+//   padding: 20px;
+//   margin: 20px;
+//   border: solid 3px;
+//   /* box-sizing: content-box; */
+//   width: 500px;
+// }
+
+// Nomeação de Varialvel, função...
+// camelCase = Toda Primeira Letra De Palavra Maiúscula
+
+// Export default, posso importar com qualquer nome e sem chaves
+// export "sem default", só posso importar entre chaves e com nome exportado (Export no final da linha do HEADER e APP)
+
+// Self closing tag
+// <Header> </Header> - Tradicional quando se vai colcoar mais coisas entre essa tag.
+// </Header> - Utilizado quando não se vai colocar mais coisas entre a tag.
+
+// Nomeação de classes em CSS
+// Metodologia BEM
+// Blocks
+// Elements
+// Modifiers
+//bloco__elemento--modificador
+// header
+// header__link
+// header__link--small
+// Nomes compostos são separados com -
+
+// Tag Vazia em REACT se chama Fragment ou Fragmento
