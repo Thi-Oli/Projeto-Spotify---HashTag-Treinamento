f46f8b57a091a900ec712065246e313bb5953cf5
Thiago Oliveira <geovanithiago08@gmail.com>
2025-04-10 23:30:47 -0300

    Primeiro commit com git

--- Projeto Spotify/src/components/Main.jsx
+++ Projeto Spotify/src/components/Main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ItemList from "./itemList";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 
